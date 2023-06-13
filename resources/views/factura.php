aa967af7b97e5f1260c91aebc4f27c647cd61acd
yli18 <2190021@ipisa.edu.do>
2023-06-12 23:33:23 -0400

    diseño de factura

--- resources/views/factura.php
+++ resources/views/factura.php
@@ -4,43 +4,53 @@
   <title>Factura</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   <style>
-    body {
-      padding: 20px;
+    body{
       background: #ddd;
     }
+    .contenedor {
+  background-color: white;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Ajusta los valores de sombra según tus preferencias */
+  margin: 0 auto; /* Centrar horizontalmente */
+  max-width: 990px; /* Ajusta el ancho máximo según tus necesidades */
+  padding: 20px; /* Ajusta el espacio interno según tus necesidades */
+  margin-top: 10%;
+  margin-bottom: 10%;
+}
 
-    .invoice {
-      border: 1px solid #ddd;
-      padding: 30px;
-      max-width: 600px;
-      margin: 0 auto;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Agrega la sombra aquí */
-      margin-top: 100px;
-      background-color: white;
-    }
+    .factura {
+  table-layout: fixed;
+}
 
-    .invoice .header {
-      text-align: center;
-      margin-bottom: 20px;
-    }
+.fact-info > div > h5 {
+  font-weight: bold;
+}
 
-    .invoice .row {
-      margin-bottom: 20px;
-    }
+.factura > thead {
+  border-top: solid 3px #000;
+  border-bottom: 3px solid #000;
+}
 
-    .invoice .row .col {
-      border: 1px solid #ddd;
-      padding: 10px;
-    }
+.factura > thead > tr > th:nth-child(2), .factura > tbod > tr > td:nth-child(2) {
+  width: 300px;
+}
 
-    .invoice .row .col:first-child {
-      width: 70%;
-    }
+.factura > thead > tr > th:nth-child(n+3) {
+  text-align: right;
+}
 
-    .invoice .row .col:last-child {
-      width: 30%;
-    }
+.factura > tbody > tr > td:nth-child(n+3) {
+  text-align: right;
+}
 
+.factura > tfoot > tr > th, .factura > tfoot > tr > th:nth-child(n+3) {
+  font-size: 24px;
+  text-align: right;
+}
+
+.cond {
+  border-top: solid 2px #000;
+}
+    
     .control-bar {
       position: fixed;
       top: 0;
@@ -56,6 +66,16 @@
     .logoholder {
        width: 14%
     }
+
+    @media (max-width: 767px) {
+  .contenedor {
+    margin: 20px;
+  }
+
+  .logoholder {
+    width: 40%;
+  }
+}
   </style>
 </head>
 <body>
@@ -64,35 +84,89 @@
      <img src="public\images\logo.png">
   </div><!--.logoholder-->
 </div>   
-  <div class="invoice">
-    <div class="header">
-        
-      <h2>Factura</h2>
+<div class="contenedor">
+  <div id="app" class="col-11">
+
+    <h2>Factura de compra</h2>
+  
+    <hr />
+  
+    <div class="row fact-info mt-3">
+      <div class="col-3">
+        <h5>Facturar a</h5>
+        <p>
+          Arian Manuel Garcia Reynoso
+        </p>
+      </div>
+      <div class="col-3">
+        <h5>Enviar a</h5>
+        <p>
+          Cotui, Sanchez Ramirez
+          Santa Fe, #19
+          arianmanuel75@gmail.com
+        </p>
+      </div>
+      <div class="col-3">
+        <h5>N° de factura</h5>
+        <h5>Fecha</h5>
+        <h5>Fecha de vencimiento</h5>
+      </div>
+      <div class="col-3">
+        <h5>103</h5>
+        <p>09/05/2019</p>
+        <p>09/05/2019</p>
+      </div>
     </div>
-    <div class="row">
-      <div class="col">
-        <p><strong>Cliente:</strong> Nombre del cliente</p>
-      </div>
-      <div class="col">
-        <p><strong>Fecha:</strong> 12 de junio de 2023</p>
-      </div>
+  
+    <div class="row my-5">
+      <table class="table table-borderless factura">
+        <thead>
+          <tr>
+            <th>Cant.</th>
+            <th>Descripcion</th>
+            <th>Precio Unitario</th>
+            <th>Importe</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Acetaminofén (Paracetamol)</td>
+            <td>3,000.00</td>
+            <td>3,000.00</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Acido valproico 200 mg/ml Solucion Oral</td>
+            <td>4,000.00</td>
+            <td>12,000.00</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th></th>
+            <th></th>
+            <th>Total Factura</th>
+            <th>RD$15,000.00</th>
+          </tr>
+        </tfoot>
+      </table>
     </div>
-    <div class="row">
-      <div class="col">
-        <p><strong>Descripción:</strong> Servicio prestado</p>
-      </div>
-      <div class="col">
-        <p><strong>Monto:</strong> $100.00</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <p><strong>Total:</strong></p>
-      </div>
-      <div class="col">
-        <p><strong>$100.00</strong></p>
+  
+    <div class="cond row">
+      <div class="col-12 mt-3">
+        <h4>Condiciones y formas de pago</h4>
+        <p>El pago se debe realizar en un plazo de 15 dias.</p>
+        <p>
+          Banco Banreserva
+          <br />
+          IBAN: DO XX 0075 XXXX XX XX XXXX XXXX
+          <br />
+          Código SWIFT: BPDODOSXXXX
+        </p>
       </div>
     </div>
   </div>
+</div> 
 </body>
 </html>
